8dd69e2c89a214457a83fdab35e7279bea8abe06
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-11-02 14:18:48 +0100

    added props to my functional component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Header /> <MainContent /> <Footer />jdhd
+    <Header /> <MainContent /> <Footer /> <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </> 
   )
 }
