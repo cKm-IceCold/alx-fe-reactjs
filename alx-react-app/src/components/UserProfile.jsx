a5af582341afdd70a5b64046bbe9a784d3b57685
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-11-02 14:20:47 +0100

    added props to my functional component correction

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -2,8 +2,8 @@ function UserProfile(props){
  return (
   <div>
     <h2>{props.name}</h2>
-    <p>Age: {props.name}</p>
-    <p>Bio: {props.age}</p>
+    <p>Age: {props.age}</p>
+    <p>Bio: {props.bio}</p>
   </div>
  );
 };
