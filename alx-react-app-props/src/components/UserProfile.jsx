5c38b729463afe8c92d17f0a980b50b0cb1d03e1
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-11-06 08:24:09 +0100

    Modified UserProfile to get useContext

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,20 @@
-function UserProfile(props){
- return (
- <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-    <h2 style={{color: 'blue' }}>{props.name}</h2>
-    <p>Age: <span style={{fontWeight: 'bold' }}> {props.age}</span></p>
-    <p>Bio: {props.bio}</p>
-  </div>
- );
-};
+import { useContext } from 'react'
+import { UserContext } from './UserContext';
+
+
+
+function UserProfile() {
+  const user = useContext(UserContext); // ✅ Get user data from context
+
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+      <h2 style={{ color: 'blue' }}>{user.name}</h2>
+      <p>
+        Age: <span style={{ fontWeight: 'bold' }}>{user.age}</span>
+      </p>
+      <p>Bio: {user.bio}</p>
+    </div>
+  );
+}
 
 export default UserProfile;
