02d48635e0c2c15a25c16900bd9195edf2a5b697
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-12-13 14:02:45 +0100

    Advanced Routing in React with React Route(correction 3)

--- react-router-advanced/src/components/ProfileDetails.jsx
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -1,4 +1,4 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
 const ProfileDetails = () => <h4>Profile Details (Nested Route)</h4>;
 export default ProfileDetails;
